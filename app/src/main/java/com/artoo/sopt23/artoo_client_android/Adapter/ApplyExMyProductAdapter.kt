4e29d5fc5b36f558e7030faff80f9860c9d1bdbe
SERIN <aloha_rini@naver.com>
2018-12-30 22:49:19 +0900

    2018-12-30 - Init
    Github

--- app/src/main/java/com/artoo/sopt23/artoo_client_android/Adapter/ApplyExMyProductAdapter.kt
+++ app/src/main/java/com/artoo/sopt23/artoo_client_android/Adapter/ApplyExMyProductAdapter.kt
@@ -1,2 +1,55 @@
 package com.artoo.sopt23.artoo_client_android.Adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.SparseBooleanArray
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RadioButton
+import com.artoo.sopt23.artoo_client_android.Data.ApplyExMyProductData
+import com.artoo.sopt23.artoo_client_android.R
+import com.bumptech.glide.Glide
+
+class ApplyExMyProductAdapter (val dataList: ArrayList<ApplyExMyProductData>): RecyclerView.Adapter<ApplyExMyProductAdapter.Holder>(){
+    lateinit var context: Context
+    var mChecked = SparseBooleanArray()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        context = parent.context
+        val view: View = LayoutInflater.from(context).inflate(R.layout.rv_item_apply_exhibition_my_product, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+        Glide.with(context).load(dataList[position].my_product_img_url).into(holder.my_product_img)
+        holder.radid_btn.isChecked = mChecked.get(position)
+
+        holder.radid_btn.setOnClickListener {
+            checkBox(position, !mChecked.get(position))
+        }
+
+
+    }
+
+    inner class Holder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val my_product_img : ImageView = itemView.findViewById<View>(R.id.img_ex_my_product) as ImageView
+        var radid_btn : RadioButton = itemView.findViewById(R.id.radio_btn_apply_ex)
+
+
+    }
+
+    fun checkBox (position: Int, value:Boolean) {
+        if(value) {
+            mChecked.put(position, true)
+            val a:Int = 0
+        }else {
+            mChecked.delete(position)
+        }
+    }
+
+}
--- app/src/main/java/com/artoo/sopt23/artoo_client_android/ApplyExhibitionActivity.kt
+++ app/src/main/java/com/artoo/sopt23/artoo_client_android/ApplyExhibitionActivity.kt
@@ -2,11 +2,42 @@ package com.artoo.sopt23.artoo_client_android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
+import com.artoo.sopt23.artoo_client_android.Adapter.ApplyExMyProductAdapter
+import com.artoo.sopt23.artoo_client_android.Data.ApplyExMyProductData
+import kotlinx.android.synthetic.main.activity_apply_exhibition.*
 
 class ApplyExhibitionActivity : AppCompatActivity() {
+
+    var applyExMyProductAdapter : ApplyExMyProductAdapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apply_exhibition)
+
+        var snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(rv_apply_ex_my_product)
+
+        var dataList: ArrayList<ApplyExMyProductData> = ArrayList()
+
+        var img_url: ArrayList<String> = ArrayList()
+        img_url.add("http://webresizer.com/images2/bird1_after.jpg")
+        img_url.add("http://img.newspim.com/news/2018/03/31/1803311718414850.jpg")
+        img_url.add("http://www.newsa.co.kr/news/photo/201809/187929_146645_3554.jpg")
+        img_url.add("https://www.nemopan.com/files/attach/images/2582/269/848/493724be0c020.jpg")
+
+        dataList.add(ApplyExMyProductData(img_url))
+        dataList.add(ApplyExMyProductData(img_url))
+        dataList.add(ApplyExMyProductData(img_url))
+        dataList.add(ApplyExMyProductData(img_url))
+        dataList.add(ApplyExMyProductData(img_url))
+        dataList.add(ApplyExMyProductData(img_url))
+
+        applyExMyProductAdapter = ApplyExMyProductAdapter(dataList)
+        rv_apply_ex_my_product.adapter = applyExMyProductAdapter
+        rv_apply_ex_my_product.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+
+
     }
 }
