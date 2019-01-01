6cc7de7a6fed853f6d69427748f9a6fa8730b80e
dshyun0226 <dshyun0226@gmail.com>
2019-01-01 18:37:46 +0900

    Bob
    Bob

---
+++ app/src/main/java/com/artoo/sopt23/artoo_client_android/JoinActivity3.kt
@@ -0,0 +1,23 @@
+package com.artoo.sopt23.artoo_client_android
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_join3.*
+
+class JoinActivity3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join3)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        btn_join_next3.setOnClickListener {
+            val intent = Intent(this, JoinActivity4::class.java)
+            startActivity(intent)
+        }
+    }
+}
