b7137c198cc9037a9d1ec72357e1dda131b7bad2
jeongeum <wjdma0103@naver.com>
2019-01-01 23:32:23 +0900

    jeongeum alarm last

--- app/src/main/java/com/artoo/sopt23/artoo_client_android/Data/ExhibitionDetailData.kt
+++ app/src/main/java/com/artoo/sopt23/artoo_client_android/Data/ExhibitionDetailData.kt
@@ -1,11 +1,2 @@
 package com.artoo.sopt23.artoo_client_android.Data
 
-data class ExhibitionDetailData(
-        var img_url:ArrayList<String>,
-        var title: String,
-        var writer: String,
-        var type : String,
-        var width : Int,
-        var height : Int,
-        var year:String
-)
